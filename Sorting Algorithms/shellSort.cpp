beaf83ded45d68cf00065e8c24a82fe5d2042fe1
Evasionn <asalyazilim@gmail.com>
2016-11-04 22:07:14 +0300

    Shell Sorting

---
+++ Sorting Algorithms/shellSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void shellSort(int arr[], int size)
+{
+	int flag = 1, i;
+	int d = size;
+	while (flag || d > 1)
+	{
+		flag = 0;
+		d = (d + 1) / 2;
+		for (i = 0; i < size - d; i++)
+		{
+			if (arr[i + d] < arr[i])
+			{
+				swap(arr[i + d], arr[i]);
+				flag = 1;
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[] = { 0,90,32,66,22,11,754,12,23,5 };
+	int size = sizeof(arr) / sizeof(int);
+	
+
+
+	shellSort(arr, size);
+	display(arr, size);
+
+	system("pause");
+}
